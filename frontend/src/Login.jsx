31a626f21dc2add63679ed71f766205030763f4a
Gargi Koul <89482614+gargi-koul@users.noreply.github.com>
2023-02-17 14:41:47 -0500

    Asthetics for Logic and Register (black and gold)

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -11,12 +11,13 @@ export const Login = (props) => {
         <div className="auth-form-container">
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Type your email here" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Type your password here" id="password" name="password" />
+                <label htmlFor="email">Email</label>
+                <input size="45" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Alex Smith" id="email" name="email" />
+                <label htmlFor="password">Password</label>
+                <input size="45" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="alexsmith@gmail.com" id="password" name="password" />
                 <button type="submit">Log In</button>
             </form>
+            
             <button className="reg-btn" onClick={() => props.onFormSwitch('register')}>Create an account</button>
         </div>
     )
--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -14,11 +14,11 @@ export const Register = (props) => {
             <h2>Create Account</h2>
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="name">Full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Type your email here" id="email" name="email" />
-            <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Type your password here" id="password" name="password" />
+            <input size="45" value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Alex Smith" />
+            <label htmlFor="email">Email</label>
+            <input size="45" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="alexsmith@gmail.com" id="email" name="email" />
+            <label htmlFor="password">Password</label>
+            <input size="45" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
             <button type="submit" onClick={() => props.onFormSwitch('login')}>Log In</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Go back to login screen</button>
