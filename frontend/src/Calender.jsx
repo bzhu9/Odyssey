5fd2f084a05e9a043f4333d33cf92e6844951266
Gargi Koul <89482614+gargi-koul@users.noreply.github.com>
2023-02-19 14:30:18 -0500

    Adding more security questions

--- frontend/src/Calender.jsx
+++ frontend/src/Calender.jsx
@@ -5,7 +5,11 @@ export const Calender = (props) => {
   return (
     <div className="App">
       <h1>Week View Calendar</h1>
+
+      <button className="reg-btn" onClick={() => props.onFormSwitch('login')}>Go back to login</button>
     </div>
+    
   );
 }
+
 
--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -20,8 +20,13 @@ export const Register = (props) => {
             <input size="45" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="alexsmith@gmail.com" id="email" name="email" />
             <label htmlFor="password">Password</label>
             <input size="80" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <label htmlFor="seq">Security Question: What is the name of the street of the first house you lived in?</label>
-            <input size="90" value={seq} onChange={(e) => setSeq(e.target.value)} type="seq" placeholder="Main Street" id="seq" name="seq" />
+            <label htmlFor="password">Security Question: What is the name of the street of the first house you lived in?</label>
+            <input size="90" value={seq} onChange={(e) => setSeq(e.target.value)} type="password" placeholder="Main Street" id="seq" name="seq" />
+            <label htmlFor="password">Security Question: What is your mother's maiden name?</label>
+            <input size="90" value={seq} onChange={(e) => setSeq(e.target.value)} type="password" placeholder="Mary" 
+            id="seq" name="seq" />
+            <label htmlFor="password">Security Question: What is your best friend's first name?</label>
+            <input size="90" value={seq} onChange={(e) => setSeq(e.target.value)} type="password" placeholder="Jackie" id="seq" name="seq" />
             <button type="submit" onClick={() => props.onFormSwitch('login')}>Log In</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Go back to login screen</button>
