02d7255051aef13072c696b4bfd5ece7b8a379e9
Sean Lee <65737799+seeenlee@users.noreply.github.com>
2023-02-23 22:39:39 -0500

    add edge case exceptions to fetch from unitime

--- classdata/executor.py
+++ classdata/executor.py
@@ -3,6 +3,19 @@ import urllib.request
 import datetime as dt
 
 def fetch_from_unitime(day_diff, filename="data.csv", start_hour=8, end_hour=20, min_size=0, max_size=500):
+    if filename[-4:] != ".csv":
+        raise Exception("file must be a csv file")
+    if day_diff < 0:
+        raise Exception("Day difference must be non-negative")
+    if day_diff == 0:
+        if start_hour > end_hour:
+            raise Exception("Start time must be before end time")
+    if start_hour > 23 or end_hour > 23:
+        raise Exception("hour must be less than 24")
+    if min_size > max_size:
+        raise Exception("max_size must be greater than min_size")
+    if min_size < 0 or max_size < 0:
+        raise Exception("Class size must be non-negative")
     current_time = dt.datetime.today()
     time_delta = dt.datetime.today() + dt.timedelta(days=day_diff)
     link = "https://timetable.mypurdue.purdue.edu/Timetabling/export?output=meetings.csv&type=room&term=Spring2023PWL&e:from={0:02d}/{1:02d}/{2}&e:to={3:02d}/{4:02d}/{5}&e:after=800&e:before=2000&r:type=genClassroom&r:size=0..500".format(current_time.month, current_time.day, current_time.year, time_delta.month, time_delta.day, time_delta.year)
