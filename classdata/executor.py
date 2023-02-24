3438aef5b63541517733db7cc19d4dad5b3b8602
Sean Lee <65737799+seeenlee@users.noreply.github.com>
2023-02-23 22:33:47 -0500

    changing fetching data into a function

--- classdata/executor.py
+++ classdata/executor.py
@@ -2,8 +2,11 @@ import pandas as pd
 import urllib.request
 import datetime as dt
 
-def fetch_from_unitime(link, filename):
-    urllib.request.urlretrieve(link, filename)
+def fetch_from_unitime(day_diff, filename="data.csv", start_hour=8, end_hour=20, min_size=0, max_size=500):
+    current_time = dt.datetime.today()
+    time_delta = dt.datetime.today() + dt.timedelta(days=day_diff)
+    link = "https://timetable.mypurdue.purdue.edu/Timetabling/export?output=meetings.csv&type=room&term=Spring2023PWL&e:from={0:02d}/{1:02d}/{2}&e:to={3:02d}/{4:02d}/{5}&e:after=800&e:before=2000&r:type=genClassroom&r:size=0..500".format(current_time.month, current_time.day, current_time.year, time_delta.month, time_delta.day, time_delta.year)
+    urllib.request.urlretrieve(link, '/classdata/%s'%filename)
 
 def clean_file(filename):
     df = pd.read_csv(filename)
@@ -18,12 +21,9 @@ def find_rooms(df):
             building[room.split()[0]].append(room.split()[1])
     return building
 
-current_time = dt.datetime.today()
-nextweek = dt.datetime.today() + dt.timedelta(days=7)
-link = "https://timetable.mypurdue.purdue.edu/Timetabling/export?output=meetings.csv&type=room&term=Spring2023PWL&e:from={0:02d}/{1:02d}/{2}&e:to={3:02d}/{4:02d}/{5}&e:after=800&e:before=2000&r:type=genClassroom&r:size=0..500".format(current_time.month, current_time.day, current_time.year, current_time.month, current_time.day, current_time.year)
-print(link)
+
 filename = "data.csv"
-fetch_from_unitime(link, filename)
+fetch_from_unitime(0, filename)
 df = pd.read_csv(filename)
 clean_file(df)
 unique_rooms = find_rooms(df)
