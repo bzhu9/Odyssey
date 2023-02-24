7b06ec1cc8fa39d261bd1728b14f8420fece17fb
Sean Lee <65737799+seeenlee@users.noreply.github.com>
2023-02-23 22:11:35 -0500

    changed date to be month/day instead of day/month

--- classdata/executor.py
+++ classdata/executor.py
@@ -7,9 +7,10 @@ import datetime as dt
 
 current_time = dt.datetime.today()
 nextweek = dt.datetime.today() + dt.timedelta(days=7)
-link = "https://timetable.mypurdue.purdue.edu/Timetabling/export?output=meetings.csv&type=room&term=Spring2023PWL&e:from={0:02d}/{1:02d}/{3}&e:to={4:02d}/{5:02d}/{6}&e:after=800&e:before=2000&r:type=genClassroom&r:size=0..50".format(current_time.day, current_time.month, current_time.year, nextweek.day, nextweek.month, nextweek.year)
-filename = "data.csv"
-fetch.fetch_from_unitime(link, filename)
+link = "https://timetable.mypurdue.purdue.edu/Timetabling/export?output=meetings.csv&type=room&term=Spring2023PWL&e:from={0:02d}/{1:02d}/{2}&e:to={3:02d}/{4:02d}/{5}&e:after=800&e:before=2000&r:type=genClassroom&r:size=0..50".format(current_time.month, current_time.day, current_time.year, nextweek.month, nextweek.day, nextweek.year)
+print(link)
+# filename = "data.csv"
+# fetch.fetch_from_unitime(link, filename)
 # df = pd.read_csv(filename)
 # clean.clean_file(df)
 # unique_rooms = uniquerooms.find_rooms(df)
