86ba007f5c6ec0c37609ce391d7d33a0e2144eb8
Sean Lee <65737799+seeenlee@users.noreply.github.com>
2023-02-23 22:20:36 -0500

    updated executor to fetch rooms with capacity up to 500

--- classdata/executor.py
+++ classdata/executor.py
@@ -7,10 +7,10 @@ import datetime as dt
 
 current_time = dt.datetime.today()
 nextweek = dt.datetime.today() + dt.timedelta(days=7)
-link = "https://timetable.mypurdue.purdue.edu/Timetabling/export?output=meetings.csv&type=room&term=Spring2023PWL&e:from={0:02d}/{1:02d}/{2}&e:to={3:02d}/{4:02d}/{5}&e:after=800&e:before=2000&r:type=genClassroom&r:size=0..50".format(current_time.month, current_time.day, current_time.year, nextweek.month, nextweek.day, nextweek.year)
+link = "https://timetable.mypurdue.purdue.edu/Timetabling/export?output=meetings.csv&type=room&term=Spring2023PWL&e:from={0:02d}/{1:02d}/{2}&e:to={3:02d}/{4:02d}/{5}&e:after=800&e:before=2000&r:type=genClassroom&r:size=0..500".format(current_time.month, current_time.day, current_time.year, current_time.month, current_time.day, current_time.year)
 print(link)
-# filename = "data.csv"
-# fetch.fetch_from_unitime(link, filename)
+filename = "data.csv"
+fetch.fetch_from_unitime(link, filename)
 # df = pd.read_csv(filename)
 # clean.clean_file(df)
 # unique_rooms = uniquerooms.find_rooms(df)
