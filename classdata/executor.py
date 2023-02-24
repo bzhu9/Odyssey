79c7d2aecfa52f7f434f9411db5bf2f1d8629f3a
Sean Lee <65737799+seeenlee@users.noreply.github.com>
2023-02-23 22:08:24 -0500

    fixed executor to fetch todays data correctly

--- classdata/executor.py
+++ classdata/executor.py
@@ -1,12 +1,15 @@
 import clean
 import fetch
+import uniquerooms
+import pandas as pd
 
 import datetime as dt
 
 current_time = dt.datetime.today()
-today = current_time.date
 nextweek = dt.datetime.today() + dt.timedelta(days=7)
-link = "https://timetable.mypurdue.purdue.edu/Timetabling/export?output=meetings.csv&type=room&term=Spring2023PWL&e:from={0:02d}/{1:02d}/{3}&e:to={4:02d}/{5:02d}/{6}&e:after=800&e:before=2000&r:type=genClassroom&r:size=0..50".format(today.day, today.month, today.year, nextweek.day, nextweek.month, nextweek.year)
+link = "https://timetable.mypurdue.purdue.edu/Timetabling/export?output=meetings.csv&type=room&term=Spring2023PWL&e:from={0:02d}/{1:02d}/{3}&e:to={4:02d}/{5:02d}/{6}&e:after=800&e:before=2000&r:type=genClassroom&r:size=0..50".format(current_time.day, current_time.month, current_time.year, nextweek.day, nextweek.month, nextweek.year)
 filename = "data.csv"
 fetch.fetch_from_unitime(link, filename)
-clean.clean_file(filename)
+# df = pd.read_csv(filename)
+# clean.clean_file(df)
+# unique_rooms = uniquerooms.find_rooms(df)
