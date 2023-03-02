86f607a72178d9db073bddd06cf1e4ec8cb63a6a
Arnav Patel <arnavpatel09@gmail.com>
2023-03-02 00:21:10 -0500

    updated modify event backend to now update all criteria

--- backend/routes/eventRoute.js
+++ backend/routes/eventRoute.js
@@ -73,8 +73,7 @@ router.route('/delete').post(async (req, res) => {
 router.route('/edit').post(async (req, res) => {
   //get the information
   const id = req.body._id;
-  //const title = req.body.title;
-  const title = "testing if this works";
+  const title = req.body.title;
   const startTime = req.body.startTime;
   const endTime = req.body.endTime;
   const location = req.body.location;
@@ -87,54 +86,35 @@ router.route('/edit').post(async (req, res) => {
   if (!title || !startTime || !endTime) {
     return res.status(400).json({ message: "All fields are required" });
   }
-
   /*
   //check if the fields are populated
   if (!title || !startTime || !endTime || !location || !users || !note || !repeating || !type || !days) {
     return res.status(400).json({ message: "All fields are required" });
   }
   */
-  /*
-    So I should be able to look it up using a unique event id
-      1. when a user clicks on an existing event, the object id is sent to the backend
-      2. the backend sends the rest of the data to the frontend
-      3. when the user finishes modifying the event, it should send the event with the rest of the rest of the data to the backend
-      4. the backend update the database accordingly
-  */
-
-  //check what this returns when the object is not updated
-  //(what if someone else deleted the event when a user was in the modify event page)
-
-  //check if the event exists
-  //if it does then update it
-  //const event = await Event.findOne({ id }).lean();
-  //res.status(200).json({ message: `Event, ${event.title}, was EDITTED` })
-  
-
-  //if (event) {
-    // can i call it on the object itself? 
-    /*
-      const result = Event.updateOne(
-        { _id:id },
-        {
-          title: title,
-          startTime: startTime, 
-          endTime: endTime, 
-          location: location, 
-          //users: users,
-          note: note,
-          //repeating: repeating,
-          //type: type,
-          //days: days
-        }
-      );
-    */
 
   const event = await Event.findOne({ id }).lean();
   if (event) {
-    event.title = title; // modify the title property of the event object
-  
-    const result = await Event.updateOne({ id }, { $set: { title: event.title } });
+    //event.title = req.body.title; // modify the title property of the event object
+    //event.startTime = req.body.startTime;
+    //event.endTime = req.body.endTime;
+    //event.location = req.body.location;
+    //const users = req.body.users;
+    //event.note = req.body.note;
+    //const repeating = req.body.repeating;
+    //const type = req.body.type;
+    //const days = req.body.days;
+    const result = await Event.updateOne({ id }, { $set: {
+      title: req.body.title,
+      startTime: req.body.startTime,
+      endTime: req.body.endTime,
+      location: req.body.location,
+      //users: req.body.users,
+      note: req.body.note,
+      //repeating: req.body.repeating,
+      //type: req.body.type,
+      //days: req.body.days 
+    } });
     res.status(200).json({ message: `Event, ediited`});
     console.log(result); // output the result of the update operation
   }
