fc959c3575b5c73b9758a4507ef342775fcc712d
Gargi Koul <89482614+gargi-koul@users.noreply.github.com>
2023-03-03 12:52:08 -0500

    ui for change email (no pass)

--- frontend/src/Settings.jsx
+++ frontend/src/Settings.jsx
@@ -4,8 +4,9 @@ import { Link, useNavigate } from "react-router-dom";
 
 export const Settings = (props) => {
     const [email, setEmail] = useState('');
+    const [emailNew, setEmailNew] = useState('');
+    const [emailNew2, setEmailNew2] = useState('');
     const [pass, setPass] = useState('');
-    const [pass2, setPass2] = useState('');
     const [seq, setSeq] = useState('');
     const navigate = useNavigate();
 
@@ -14,38 +15,38 @@ export const Settings = (props) => {
     }
 
     const deleteUser = async () => {
-        console.log("USER: ");
-        console.log(sessionStorage.getItem("user"));
-        if (sessionStorage.getItem("user") == null) {
-            console.log("wtf?")
-            window.alert("Please login delete your account!");
-        }
-        else if (email !== sessionStorage.getItem("user")){
-            window.alert("Please provide the email associated to your account");
-        }
-        else if (pass !== pass2) {
-            window.alert("Passwords do not match");
-        }
-        else {
-            // console.log(sessionStorage.getItem("user"));
-            const payload = {
-                email: sessionStorage.getItem("user"),
-                password: pass,
-                seq: seq,
-            };
-            // const formEmail = email;
-            await api.deleteUser(payload).then( async res => {
-                // console.log(res);
-                alert("Account deleted successfully");
-                sessionStorage.removeItem("user");
-                navigate("../login");
-            }).catch (function (error) {
-                if (error.response) {
-                    alert(error.response.data.message);
-                }
-            })
-            // props.onFormSwitch('login');
-        }
+        // console.log("USER: ");
+        // console.log(sessionStorage.getItem("user"));
+        // if (sessionStorage.getItem("user") == null) {
+        //     console.log("wtf?")
+        //     window.alert("Please login delete your account!");
+        // }
+        // else if (email !== sessionStorage.getItem("user")){
+        //     window.alert("Please provide the email associated to your account");
+        // }
+        // else if (pass !== pass2) {
+        //     window.alert("Passwords do not match");
+        // }
+        // else {
+        //     // console.log(sessionStorage.getItem("user"));
+        //     const payload = {
+        //         email: sessionStorage.getItem("user"),
+        //         password: pass,
+        //         seq: seq,
+        //     };
+        //     // const formEmail = email;
+        //     await api.deleteUser(payload).then( async res => {
+        //         // console.log(res);
+        //         alert("Account deleted successfully");
+        //         sessionStorage.removeItem("user");
+        //         navigate("../login");
+        //     }).catch (function (error) {
+        //         if (error.response) {
+        //             alert(error.response.data.message);
+        //         }
+        //     })
+        //     // props.onFormSwitch('login');
+        // }
     }
 
     return (
@@ -54,12 +55,13 @@ export const Settings = (props) => {
         <form className="login-form" onSubmit={handleSubmit}>
             <label htmlFor="email">Email</label>
             <input size="45" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="alexsmith@gmail.com" id="email" name="email" />
-            <label htmlFor="seq">Security Question: What is the name of the street of the first house you lived in?</label>
-            <input size="90" value={seq} onChange={(e) => setSeq(e.target.value)} type="seq" placeholder="Main Street" id="seq" name="seq" />
-            <label htmlFor="password"> Password</label>
-            <input size="45" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <label htmlFor="password">Confirm Password</label>
-            <input size="80" value={pass2} onChange={(e) => setPass2(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <label htmlFor="email">New Email</label>
+            <input size="45" value={emailNew} onChange={(e) => setEmailNew(e.target.value)} type="email" placeholder="alexsmith2@gmail.com" id="email" name="email" />
+            <label htmlFor="email">Confirm New Email</label>
+            <input size="45" value={emailNew2} onChange={(e) => setEmailNew2(e.target.value)} type="email" placeholder="alexsmith2@gmail.com" id="email" name="email" />
+            {/* <label htmlFor="password"> Password</label>
+            <input size="45" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" /> */}
+            <button type="submit">Change Email</button>
             <button type="submit" onClick={deleteUser}>Delete Account</button>
         </form>
         {/* <button className="link-btn" onClick={() => props.onFormSwitch('reset')}>Change Password</button> */}
