7de1f592d1d97af6d5f274bb8bffd20063bd0794
Gargi Koul <89482614+gargi-koul@users.noreply.github.com>
2023-03-12 16:52:01 -0400

    basic UI for seeing friends

--- frontend/src/Friends.jsx
+++ frontend/src/Friends.jsx
@@ -2,6 +2,35 @@ import React, { useState } from "react";
 import api from "./apis"
 import { Link, useNavigate } from "react-router-dom";
 
+const list = [
+    {
+      id: '1',
+      firstname: 'Daniel',
+      lastname: 'McConnell',
+    },
+    {
+      id: 'b',
+      firstname: 'Olga R',
+      lastname: 'Gibson',
+    },
+    {
+        id: 'b',
+        firstname: 'Olga R',
+        lastname: 'Gibson',
+      },
+      {
+        id: 'b',
+        firstname: 'Olga R',
+        lastname: 'Gibson',
+      },
+      {
+        id: 'b',
+        firstname: 'Olga R',
+        lastname: 'Gibson',
+      },
+    
+  ];
+
 export const Friends = (props) => {
     // const [email, setEmail] = useState('');
     // const [emailNew, setEmailNew] = useState('');
@@ -14,22 +43,37 @@ export const Friends = (props) => {
         e.preventDefault();
     }
 
+    
+    
 
     return (
         <div>
+            
        <Link to="/settings">
             <button size="45" className="settingsNav-btn" >Profile</button>
         </Link>
         <Link to="/friends">
             <button size="45" className="friendNav-btn" >Friends</button>
         </Link>
-        <div className="auth-form-container">
-            <h2>Friends Settings</h2>
-        
-        {/* <button className="link-btn" onClick={() => props.onFormSwitch('reset')}>Change Password</button> */}
-        {/* <button className="link-btn" onClick={() => props.onFormSwitch('calender')}>Go back to Calender</button> */}
+        <div >
+            <h2 >Friends Settings</h2>
+            <h4 className="friendsTitle">Current Friends</h4>
+            <ul className="friendsList">
+            {list.map(item => {
+            const ref = React.createRef();
+            return (
+                <li key={item.id} ref={ref} >
+                <div>{item.firstname} {item.lastname} {item.id}</div>
+                </li>
+                );
+             })}
+         </ul>
+         
         <Link to="/cal">
             <button size="45" className="reset-btn" type="submit">Weekly View</button>
+        </Link>
+        <Link to="/cal">
+            <button size="45" className="reset-btn" type="submit">Add Friend</button>
         </Link>
 
     </div>
