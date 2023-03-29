e59e5d44ceca1fcd748c83fd11ae58a82921690d
Sean Lee <65737799+seeenlee@users.noreply.github.com>
2023-03-29 01:45:52 -0400

    end time is now also used to find classroom

--- backend/routes/openClassRoute.js
+++ backend/routes/openClassRoute.js
@@ -3,35 +3,71 @@ let OpenClass = require("../models/OpenClass");
 
 // GET
 router.route("/").get((req, res) => {
-    // res.send("hi")
-    // OpenClass.find({building: req.body.building}, {startTime: req.body.startTime}, {endTime: req.endTime}, {room: req.body.room}, (err, rooms) => {
-    //     if (err) return handleError(err);
-    //     res.send(rooms)
+    // // res.send("hi")
+    // // OpenClass.find({building: req.body.building}, {startTime: req.body.startTime}, {endTime: req.endTime}, {room: req.body.room}, (err, rooms) => {
+    // //     if (err) return handleError(err);
+    // //     res.send(rooms)
+    // // })
+    // // somehow find all the times that match
+    // // OpenClass.find()
+    // OpenClass.find({
+    //     // startTime: { $lt: req.body.startTime},
+    //     // endTime: {$gt: req.body.endTime},
+    //     building: req.body.building
     // })
-    // somehow find all the times that match
-    // OpenClass.find()
+    // .then(room => res.json(room))
+    // .catch(err => res.status(400).json("Error: " + err));
+    // // OpenClass.aggregate([
+    // //     { $unwind: "$OpenClass" },
+    // //     { $project: { }}
+    // // ])
+    // // res.send('looking')
+    point
+    OpenClass.findOne({building: req.body.building}, {geoPoint : 1})
+    .then(doc => point = doc.geoPoint)
+    .catch(err => res.status(400).json("Error: " + err));
+
     OpenClass.find({
-        // startTime: { $lt: req.body.startTime},
-        // endTime: {$gt: req.body.endTime},
-        building: req.body.building
+        endTime: {$gt: req.body.endTime},
+        startTime: {$lt: req.body.startTime},
+        location: {
+            $near: {
+                $geometry: point
+            }
+        }
     })
     .then(room => res.json(room))
-    .catch(err => res.status(400).json("Error: " + err));
-    // OpenClass.aggregate([
-    //     { $unwind: "$OpenClass" },
-    //     { $project: { }}
-    // ])
-    // res.send('looking')
+    .catch(err => rest.status(400).json("error: " + err));
 });
 
 router.route("/").post((req, res) => {
     OpenClass.find({
         startTime: { $lt: req.body.startTime},
-        // endTime: {$gt: req.body.endTime},
+        endTime: {$gt: req.body.endTime},
         building: req.body.building
     })
     .then(room => res.json(room))
     .catch(err => res.status(400).json("Error: " + err));
+
+    // OpenClass.find({building: req.body.building}, {geoPoint : 1})
+    // OpenClass.find({
+    //     building: req.body.building
+    // })
+    // // .then(doc => point = doc.geoPoint)
+    // .then(room => res.json(room))
+    // .catch(err => res.status(400).json("Error: " + err));
+
+    // OpenClass.find({
+    //     endTime: {$gt: req.body.endTime},
+    //     startTime: {$lt: req.body.startTime},
+    //     location: {
+    //         $near: {
+    //             $geometry: point
+    //         }
+    //     }
+    // })
+    // .then(room => res.json(room))
+    // .catch(err => rest.status(400).json("error: " + err));
 });
 
 router.route("/all").get((req, res) => {
