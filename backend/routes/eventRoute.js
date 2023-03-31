065165e912be041a94e25377b54da33654b02bc7
Arnav Patel <arnavpatel09@gmail.com>
2023-03-31 03:09:24 -0400

    cleaned up eventRequest routes

--- backend/routes/eventRoute.js
+++ backend/routes/eventRoute.js
@@ -231,6 +231,10 @@ router.route('/edit').post(async (req, res) => {
 });
 
 
+
+
+
+// Accept an event request -----------------------------------
 router.route("/acceptEventRequest").post(async (req, res) => {
   const email = req.body.email;
   const eventID = req.body.id;
@@ -242,8 +246,6 @@ router.route("/acceptEventRequest").post(async (req, res) => {
   
   const event = await Event.findOne({_id: eventID})
       .catch(err => res.status(400).json("Error: " + err));
-
-  //console.log(event);
   
   //convert objects into strings and then find and remove
   user.req_events.splice(user.req_events.indexOf(event._id), 1)
@@ -265,6 +267,11 @@ router.route("/acceptEventRequest").post(async (req, res) => {
   return res.status(200).json({ message: `Event: ${event.title} sucessfully accepted`});
 });
 
+
+
+
+
+// Delete an event request -----------------------------------
 router.route("/deleteEventRequest").post(async (req, res) => {
   const email = req.body.email;
   const eventID = req.body.id;
@@ -280,26 +287,18 @@ router.route("/deleteEventRequest").post(async (req, res) => {
       .catch(err => res.status(400).json("Error: " + err));
   
   //remove from req_event in user object
-  console.log(user.req_events);
   user.req_events.splice(user.req_events.indexOf(event._id), 1)
-  console.log(user.req_events);
-
-  console.log("============");
   
   //remove it from the req user list in event object
-  console.log(event.req_users);
   event.req_users.splice(event.req_users.indexOf(user._id), 1);
-  console.log(event.req_users);
 
 
   //save the user and event
-  /*
   user.save()
       .catch(err => res.status(400).json({message: 'accept invite user Error: ' + err}));
   event.save()
       .catch(err => res.status(400).json({message: 'accept invite event Error: ' + err}));
 
-  */
 
   return res.status(200).json({ message: `Event: ${event.title} deleted!`});
 });
