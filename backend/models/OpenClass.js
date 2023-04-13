e11a3f6bd3213a8d4791e5bc8dcf1b8e3aeca87e
Sean Lee <65737799+seeenlee@users.noreply.github.com>
2023-04-13 00:31:37 -0400

    cleaned up openclass.js

--- backend/models/OpenClass.js
+++ backend/models/OpenClass.js
@@ -52,47 +52,6 @@ const openClassSchema = new Schema({
         type: Number,
         require: true,
     }
-
-    // name: {
-    //     type: String,
-    //     require: false,
-    // },
-    // section: {
-    //     type: String,
-    //     require: false,
-    // },
-    // startTime: {
-    //     type: Date,
-    //     require: true,
-    // },
-    // endTime: {
-    //     type: Date,
-    //     require: true,
-    // },
-    // building: {
-    //     type: String,
-    //     require: true,
-    // },
-    // room: {
-    //     type: String,
-    //     require: true,
-    // },
-    // capacity: {
-    //     type: Number,
-    //     require: true,
-    // },
-    // address: {
-    //     type: String,
-    //     require: false,
-    // },
-    // lat: {
-    //     type: Number,
-    //     require: true,
-    // },
-    // long: {
-    //     type: Number,
-    //     require: true,
-    // }
 });
 
 const OpenClass = mongoose.model("OpenClass", openClassSchema);
